246dc068723c9db3bb45af6f1dcaa78e51841913
Andrew <andrew@lobos.me>
2014-03-24 08:55:27 -0400

    Upgrade to sails v0.10

---
+++ config/connections.js
@@ -0,0 +1,46 @@
+/**
+ * Connections
+ * 
+ * `Connections` are like "saved settings" for your adapters.  What's the difference between
+ * a connection and an adapter, you might ask?  An adapter (e.g. `sails-mysql`) is generic--
+ * it needs some additional information to work (e.g. your database host, password, user, etc.) 
+ * A `connection` is that additional information.
+ * 
+ * Each model must have a `connection` property (a string) which is references the name of one
+ * of these connections.  If it doesn't, the default `connection` configured in `config/models.js`
+ * will be applied.  Of course, a connection can (and usually is) shared by multiple models.
+ * .
+ * Note: If you're using version control, you should put your passwords/api keys 
+ * in `config/local.js`, environment variables, or use another strategy.
+ * (this is to prevent you inadvertently sensitive credentials up to your repository.)
+ *
+ * For more information on configuration, check out:
+ * http://links.sailsjs.org/docs/config/connections
+ */
+
+module.exports.connections = {
+
+  // Persistent adapter for DEVELOPMENT ONLY
+  // (data is preserved when the server shuts down)
+  disk: {
+    module: 'sails-disk'
+  },
+  
+  mongo: {
+  		module   : 'sails-mongo',
+  		url      : process.env.MONGOHQ_URL || 'mongodb://localhost/sails'
+  },
+
+  // MySQL is the world's most popular relational database.
+  // Learn more: http://en.wikipedia.org/wiki/MySQL
+  myLocalMySQLDatabase: {
+
+    module: 'sails-mysql',
+    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_MYSQL_USER',
+    // Psst.. You can put your password in config/local.js instead
+    // so you don't inadvertently push it up if you're using version control
+    password: 'YOUR_MYSQL_PASSWORD', 
+    database: 'YOUR_MYSQL_DB'
+  }
+};
